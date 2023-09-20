ecc0787b2ec8040a9f481da51d200ed28f2132ed
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-09-20 16:26:11 -0300

    Feat: Pages structures
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Home } from './pages/Home';
+
 export function App() {
-  return <h1 className="text-3xl font-poppins font-bold">Hello world!</h1>;
+  return <Home />;
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <main className=" min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-poppins font-bold">Hello world!</h1>
+    </main>
+  );
+}
