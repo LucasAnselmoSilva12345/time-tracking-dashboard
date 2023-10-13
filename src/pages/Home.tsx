b8736f6bf30efa265f4326d285f7b3ed77bd40c8
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-10-13 19:04:16 -0300

    Feat: Display showed JSON data according to the user's choice
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import { UserProfile } from '../components/UserProfileCard/UserProfile';
+import dataJSON from '../lib/data.json';
+
+type DataItem = {
+  title: string;
+  timeframes: Record<string, { current: number; previous: number }>;
+};
+
+type TimeframeOption = 'daily' | 'weekly' | 'monthly';
 
 export function Home() {
+  const [selectedOption, setSelectedOption] =
+    useState<TimeframeOption>('daily');
+
+  const handleOptionChange = (option: TimeframeOption) => {
+    setSelectedOption(option);
+  };
+
+  const selectedData = dataJSON.map((item: DataItem) => ({
+    title: item.title,
+    current: item.timeframes[selectedOption].current,
+    previous: item.timeframes[selectedOption].previous,
+  }));
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center md:flex-row">
-      <UserProfile />
-      <h1 className="text-3xl font-poppins font-bold">Hello world!</h1>
+      <UserProfile
+        selectedOption={selectedOption}
+        handleOptionChange={handleOptionChange}
+      />
+      <div>
+        {selectedData.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <p>{item.current}</p>
+            <p>{item.previous}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
